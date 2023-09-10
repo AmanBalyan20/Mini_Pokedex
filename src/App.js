caaa84023baad7d2d5bf7a7a6ea7288f180c1f8e
AmanBalyan20 <19ca1001@mvn.edu.in>
2023-09-10 16:45:56 +0530

    V1.0

--- src/App.js
+++ src/App.js
@@ -24,10 +24,10 @@ function App() {
   }, []); // Empty dependency array ensures this runs only once on component mount
 
 
-//Fetch the API
+  //Fetch the API
 
-const[allPokemons, setAllPokemons] = useState([])
-   const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+  const [allPokemons, setAllPokemons] = useState([])
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
 
   const getAllPokemons = async () => {
     const res = await fetch(loadMore)
@@ -35,31 +35,28 @@ const[allPokemons, setAllPokemons] = useState([])
 
     setLoadMore(data.next)
 
-    function createPokemonObject(results)  {
-      results.forEach( async pokemon => {
+    function createPokemonObject(results) {
+      results.forEach(async pokemon => {
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        const data =  await res.json()
-        setAllPokemons( currentList => [...currentList, data])
+        const data = await res.json()
+        setAllPokemons(currentList => [...currentList, data])
         await allPokemons.sort((a, b) => a.id - b.id)
       })
     }
     createPokemonObject(data.results)
   }
 
- useEffect(() => {
-  getAllPokemons()
- }, [])
-
-
-
+  useEffect(() => {
+    getAllPokemons()
+  }, [])
 
   return (
     <div className="App">
       <Heading />
-      <Filter/>
+      <Filter />
       <div className="app-contaner">
         <div className="all-container">
-          {allPokemons.map( (pokemonStats, index) => 
+          {allPokemons.map((pokemonStats, index) =>
             <Cards
               key={index}
               id={pokemonStats.id}
@@ -67,10 +64,10 @@ const[allPokemons, setAllPokemons] = useState([])
               name={pokemonStats.name}
               type={pokemonStats.types[0].type.name}
             />)}
-          
+
         </div>
-          <button className="load-more" onClick={() => getAllPokemons()}>Load more</button>
-    </div>
+        <button className="load-more" onClick={() => getAllPokemons()}>Load more</button>
+      </div>
     </div>
   );
 }
